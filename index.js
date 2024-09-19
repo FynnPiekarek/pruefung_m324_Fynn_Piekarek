19f171bc32133bd80645653ff9c162240f8d8467
Fynn Piekarek <fynn.piekarek.2005@gmail.com>
2024-09-19 10:53:58 +0200

    linter test

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const chalk = require('chalk');
-console.log(chalk.blue('It works'));
+
+// Remove this if unused, or use it in the program
+let myUnusedVariable = 42; // Unused variable that should either be used or removed
+
+
+function checkNumber(num) {
+  if (num == 10) {  
+    console.log(chalk.green('The number is 10.'));
+  } else {
+    console.log(chalk.red('The number is not 10.'));
+  }
+}
+
+checkNumber(10);
